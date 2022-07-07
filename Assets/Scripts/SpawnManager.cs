690f1639cf84069a6843b2549ab2e46b1cc4de0e
VladSith <swkotorsotton@gmail.com>
2022-07-07 10:58:48 +0300

    StructChanges
    Structure and script Changes

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform SpawnPosRed;
+    [SerializeField]
+    private GameObject RedCrips;
+    [SerializeField]
+    private float TimeSpawnRed;
+
+    [SerializeField]
+    private Transform SpawnPosGreen;
+    [SerializeField]
+    private GameObject GreenCrip;
+    [SerializeField]
+    private float TimeSpawnGreen;
+
+    [SerializeField]
+    private Transform SpawnPosBlue;
+    [SerializeField]
+    private GameObject BlueCrip;
+    [SerializeField]
+    private float TimeSpawnBlue;
+
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnCDRed());
+        StartCoroutine(SpawnCDGreen());
+        StartCoroutine(SpawnCDBlue());
+    }
+
+    void RepeatBlue()
+    {
+        StartCoroutine(SpawnCDBlue());
+    }
+    IEnumerator SpawnCDBlue()
+    {
+        yield return new WaitForSeconds(TimeSpawnBlue);
+        Instantiate(BlueCrip, SpawnPosBlue.position, Quaternion.identity);
+        RepeatBlue();
+    }
+
+
+    void RepeatGreen()
+    {
+        StartCoroutine(SpawnCDGreen());
+    }
+    IEnumerator SpawnCDGreen()
+    {
+        yield return new WaitForSeconds(TimeSpawnGreen);
+        Instantiate(GreenCrip, SpawnPosGreen.position, Quaternion.identity);
+        RepeatGreen();
+    }
+
+    void RepeatRed()
+    {
+        StartCoroutine(SpawnCDRed());
+    }
+    IEnumerator SpawnCDRed()
+    {
+        yield return new WaitForSeconds(TimeSpawnRed);
+        Instantiate(RedCrips, SpawnPosRed.position, Quaternion.identity);
+        RepeatRed();
+    }
+}
